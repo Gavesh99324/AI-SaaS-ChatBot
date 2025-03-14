27a15d8432c2dd4a6cfbe7f956836b3ad66dcef3
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-03-14 11:37:23 +0530

    update

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,17 +1,3 @@
-import express from 'express';
-import { config } from '.env';
-config()
-const app = express();
-
-// Middlewares
-app.use(express.json());
-
-
-// Connections and  Listeners
-app.listen(5000, () => console.log("Server Open"));
-
-
-
 
 
 
---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import app from "./app.js";
+
+// Connections and  Listeners
+app.listen(5000, () => console.log("Server Open"));
+
+
+
+
+
+
+
+
+
+
+
+
