a5a665abeff4934ffe9500f54972c60f1d9ca130
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-03-23 15:29:11 +0530

    update frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const theme = createTheme({ 
@@ -14,8 +15,11 @@ const theme = createTheme({
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-    <App />
-    </ThemeProvider>
+    <BrowserRouter>
+     <ThemeProvider theme={theme}>
+      <App />
+     </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>
 )
+
