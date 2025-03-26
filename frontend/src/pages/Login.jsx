a6604b8aaa005a91940eedd9d1414a6d732ccfc4
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-03-26 11:59:36 +0530

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import Robot from '../../src/assets/robot_p.png';
+
+
+
 
 const Login = () => {
+  console.log("Robot Image Path:", Robot);
   return (
-    <div>
-      Login
-    </div>
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <h1>Login</h1>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none"}}>
+        <img src={Robot} alt="Robot" style={{ width: "400px" }} />
+      </Box>
+      <Box display={"flex"} flex={{ xs: 1, md: 0.5 }} justifyContent={'center'} alignItems={'center'} padding={2} ml={"auto"} mt={16}>
+        <form style={{ margin: 'auto', padding: '30px', boxShadow: "10px 10px 20px #000", borderRadius: "10px", border: "none", }}>
+          <Box sx={{ display: 'flex', flexDirection:"column", justifyContent: "center" }}>
+            <Typography></Typography>
+          </Box>
+        </form>
+      </Box>
+    </Box>
   )
-}
+};
 
-export default Login
+export default Login;
+
+
