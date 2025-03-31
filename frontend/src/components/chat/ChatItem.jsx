627c35383dc0e81b3a46ac6193382dfdcc36cc08
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-03-31 16:50:41 +0530

    update

---
+++ frontend/src/components/chat/ChatItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Box, Avatar, Typography } from '@mui/material';
+import { useAuth } from '../../context/AuthContext';
+import Logo from '../../assets/Logo.png'; 
+
+
+function ChatItem({ content, role }) {
+    const auth = useAuth();
+
+  return role === "assistant" ? (
+    <Box sx={{ display: "flex", p: 2, bgcolor: "rgba(0, 77, 86, 0.5)", my: 2, gap: 2 }}
+    >
+      <Avatar sx={{ ml: "0" }}>
+        <img src={Logo} alt="neurabot" width={"30px"} />
+      </Avatar>
+      <Box>
+        <Typography fontSize={"15px"}>{content}</Typography>
+      </Box>
+    </Box>
+  ) : (
+    <Box sx={{ display: "flex", p: 2, bgcolor: "rgba(0, 77, 86, 0.1)", my: 2, gap: 2, borderRadius: "10px" }}
+    >
+      <Avatar sx={{ ml: "0", bgcolor: "black", color: "white"}}>
+      { auth?.user?.name[0] || "?" }
+      { auth?.user?.name.split(" ")[1][0] }
+      </Avatar>
+      <Box>
+        <Typography fontSize={"15px"}>{content}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default ChatItem;
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,21 +1,60 @@
 import React from 'react';
-import { Avatar, Box } from '@mui/material';
+import { Avatar, Box, Button, IconButton, Typography } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
+import { red, orange } from '@mui/material/colors';
+import ChatItem from '../components/chat/ChatItem';
+import { IoMdSend } from 'react-icons/io';
+
+const chatMessages = [
+  { role: "user", content: "Hello!" },
+  { role: "assistant", content: "Hi there! How can I assist you today?" },
+  { role: "user", content: "Tell me a joke." },
+  { role: "assistant", content: "Sure! Why don’t skeletons fight each other? Because they don’t have the guts!" },
+  { role: "user", content: "That was funny! Can you recommend a book?" },
+  { role: "assistant", content: "Of course! 'Atomic Habits' by James Clear is a great book on building good habits and breaking bad ones." }
+];
+
 
 const Chat = () => {
   const auth = useAuth();
 
   return (
-    <Box sx={{display: "flex", flex: 1, width: "100%", height: "100%", mt: 3, gap: 3 }}>
-      <Box sx={{ display: { md: "flex", sm: "none", xs: "none"}}}>
-        <Box sx={{ display: "flex", width: "100%", height: "60vh", bgcolor: "rgb(17, 29, 39)", borderRadius: 5, flexDirection: "column", mx: 3}}>
+    <Box sx={{ display: "flex", flex: 1, width: "100%", height: "100%", mt: 3, gap: 3, paddingBottom: "60px" }}>
+      <Box sx={{ display: { md: "flex", sm: "none", xs: "none"}, flex: 0.2, flexDirection: "column" }}>
+        <Box sx={{ display: "flex", width: "100%", height: "100vh", bgcolor: "rgb(17, 29, 39)", borderRadius: 5, flexDirection: "column", mx: 3}}>
           <Avatar sx={{ mx: "auto", my: 2, bgcolor: "white", color: "black", fontWeight: 700}}>
             { auth?.user?.name[0] || "?" }
+            { auth?.user?.name.split(" ")[1][0] }
           </Avatar>
+          <Typography sx={{ mx: 'auto', fontFamily: "work-sans, sans-serif"}}>
+            You are talking to a NeuraBot
+          </Typography>
+          <Typography sx={{ mx: 'auto', fontFamily: "work-sans, sans-serif", my: 4, p: 3}}>
+            You can ask some questions related to knowledge, Business, Advices, Education, etc. But avoid sharing personal information.
+          </Typography>
+          <Button sx={{ width: "200px", my: 'auto', color: 'white', fontWeight: "700", borderRadius: 3, mx: "auto", bgcolor: orange[300], ":hover": {bgcolor: red.A400}}}>
+            Clear Conversation
+          </Button>
         </Box>
       </Box>
+      <Box sx={{ display: "flex", flex: { md: 0.8, xs: 1, sm: 1 },flexDirection: 'column', px: 3 }}>
+        <Typography sx={{ fontSize: "40px", color: "white", mb: 2, mx: "auto", fontWeight: "600" }}>
+          How Can I Help You?
+        </Typography>
+        <Box sx={{ width: "90%", borderRadius: 3, mx: "auto", display: 'flex', flexDirection: "column", overflowX: "hidden", overflowY: "auto", scrollBehavior: "smooth" }}>
+          {chatMessages.map((chat, index) => (
+              <ChatItem content={chat.content} role={chat.role} key={index}/> 
+            ))}
+        </Box>
+        <div style={{ width: "90%", marginBottom: "60px", marginTop: "auto", padding: "10px", borderRadius: 16, backgroundColor: "rgb(17, 29, 39", display: "flex", margin: "auto", flexDirection: "column"}}
+        >
+          {" "}
+          <input type='text' style={{ width: "90%", height: "10px", backgroundColor: "transparent", marginBottom: "20px", padding: "5px", border: "none", outline: "none", color: "white", fontSize: "15px" }} />
+          <IconButton sx={{ ml: "auto", color: "white" }}><IoMdSend /></IconButton>
+        </div>
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default Chat;
