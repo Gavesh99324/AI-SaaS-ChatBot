b0d02ceed8679793cf69e34d1dbdb500fc59f54b
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-04-01 15:28:28 +0530

    update

--- frontend/src/components/shared/Logo.jsx
+++ frontend/src/components/shared/Logo.jsx
@@ -1,35 +1,3 @@
-/*
-
-import { Typography } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import chatgpt from '../../assets/chatgpt.png';
-
-const Logo = () => {
-  return (
-    <div style={{ display: 'flex', marginRight: 'auto', marginTop: '10px', alignItems: 'center', gap: '15px'}}>
-      <Link to={"/"}>
-       <img src={chatgpt} alt="openai" width={"75px"} height={"45px"} className='image-inverted' />
-      </Link>
-       <Typography 
-           sx={{ 
-            display: { md: "block", sm: "none", xs: "none"},
-            mr: "auto", fontWeight: "800", 
-            textShadow: "2px 2px 2px #000"
-            }}>
-            <span style={{ fontSize: "20px" }}>NeuraBot</span>
-       </Typography>
-    </div>
-  );
-}; 
-
-export default Logo;
-
-*/
-
-
-
-
 
 import React from 'react';
 import { Link } from 'react-router-dom';
